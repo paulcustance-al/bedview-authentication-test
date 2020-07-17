20407fd477ee9d6d02fa2228d08384644febea4a
Paul Custance <paul.custance@airelogic.com>
2020-07-17 16:11:38 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace test_wa.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }   
+
+        public IActionResult Index()
+        {
+            ViewData["authenticated"] = User.Identity.IsAuthenticated;
+            ViewData["name"] = User.Identity.Name;
+
+            return View();
+        }
+    }
+}
