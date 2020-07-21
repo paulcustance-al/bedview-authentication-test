32a3490cf23b664ece9af605db29b29080fa2e16
Paul Custance <paul.custance@airelogic.com>
2020-07-21 16:32:38 +0100

    Add impersonation code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Principal;
+using System.Threading.Tasks;
 
 namespace test_wa.Controllers
 {
@@ -18,6 +20,28 @@ namespace test_wa.Controllers
         {
             ViewData["authenticated"] = User.Identity.IsAuthenticated;
             ViewData["name"] = User.Identity.Name;
+
+            // The user used as Log On as for the Windows Service
+            var serviceUser = WindowsIdentity.GetCurrent().Name;
+
+            ViewData["serviceUser"] = serviceUser;
+    
+            // The user to be impersonated
+            var userToImpersonate = (WindowsIdentity)HttpContext.User.Identity;
+
+            ViewData["userToImpersonate"] = userToImpersonate;
+
+            // Impersonating the current Windows user [HttpContext.User.Identity]...
+            WindowsIdentity.RunImpersonated(userToImpersonate.AccessToken, () =>
+            {
+                // This time WindowsIdentity.GetCurrent() will retrieve the impersonated
+                // user with its claims...
+                var impersonatedUser = WindowsIdentity.GetCurrent().Name;
+
+                ViewData["impersonatedUser"] = impersonatedUser;
+
+                // Your business logic code here...  
+            });
 
             return View();
         }
